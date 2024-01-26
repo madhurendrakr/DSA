b67c199b5078484b1bb39e40cd784807112e12a2
madhurendrakr <88264792+madhurendrakr@users.noreply.github.com>
2024-01-26 23:27:43 +0530

    program -->  leetcode 576.  out of boudary path

--- type.cpp
+++ type.cpp
@@ -1,24 +1,27 @@
-class Solution
-{
+class Solution {
 public:
-    int dfs(int i, vector<string> &arr, vector<int> vec)
-    {
-        if (i == arr.size())
-            return 0;
-        int skip = dfs(i + 1, arr, vec);
-        for (char c : arr[i])
-        {
-            if (vec[c - 'a'])
-                return skip;
-            vec[c - 'a'] = 1;
-        }
-        int temp = arr[i].length() + dfs(i + 1, arr, vec);
-        return max(skip, temp);
+       
+     int mod = 1e9 + 7;
+    map<vector<int>, int> temp;
+
+    long long dfs(int startRow, int startColumn, int m, int n, int mm){
+        if(mm == 0)
+            return (startRow < 0 || startColumn < 0 || startRow >= m || startColumn >= n);
+        if(startRow < 0 || startColumn < 0 || startRow >= m || startColumn >= n) 
+            return 1;
+
+        if(temp.count({startRow, startColumn, mm})) return temp[{startRow, startColumn, mm}];
+        return temp[{startRow, startColumn, mm}] = (long long)(
+            (dfs(startRow-1, startColumn, m, n, mm-1)%mod)+
+            (dfs(startRow+1, startColumn, m, n, mm-1)%mod)+
+            (dfs(startRow, startColumn-1, m, n, mm-1)%mod)+
+            (dfs(startRow, startColumn+1, m, n, mm-1)%mod)
+        ) % mod;
     }
 
-    int maxLength(vector<string> &arr)
-    {
-        vector<int> vec(26, 0);
-        return dfs(0, arr, vec);
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        int  ans = dfs(startRow, startColumn, m, n, maxMove)%mod;
+
+        return ans ;
     }
 };
